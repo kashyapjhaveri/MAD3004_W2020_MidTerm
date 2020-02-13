4d17344498b47f3010d5f9b58f54200e71137529
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-13 18:09:21 -0500

    [Kashyap jhaveri] Created all files and the inheritance structure

---
+++ StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,14 @@
+//
+//  Mobile.swift
+//  StudentID_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 Kashyap Jhaveri. All rights reserved.
+//
+
+import Foundation
+
+class Mobile: Bill {
+    
+    
+}
