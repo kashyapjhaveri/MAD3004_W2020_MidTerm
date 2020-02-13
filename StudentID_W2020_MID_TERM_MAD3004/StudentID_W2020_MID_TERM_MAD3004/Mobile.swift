90620f22ac8a634d0961d2a544dead66e4f8e761
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-13 18:34:33 -0500

    [Kashyap Jhaveri] Created iniitmethod for mobile class and override display method

--- StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/Mobile.swift
+++ StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/Mobile.swift
@@ -14,4 +14,17 @@ class Mobile: Bill {
     var mobileNumber:String;
     var gbUsed:Float;
     var minuteUsed:Float;
+    
+    init(_ billId:Int,_ billDate:Date,_ billType:String,_ totalAmount:Double,_ manufacturerName:String,_ planName:String,_ mobileNumber:String,_ gbUsed:Float,_ minuteUsed:Float) {
+        self.manufacturerName=manufacturerName;
+        self.planName=planName;
+        self.mobileNumber=mobileNumber;
+        self.gbUsed=gbUsed;
+        self.minuteUsed=minuteUsed;
+        super.init(billId, billDate, billType, totalAmount);
+    }
+    
+    override func Display() {
+        
+    }
 }
