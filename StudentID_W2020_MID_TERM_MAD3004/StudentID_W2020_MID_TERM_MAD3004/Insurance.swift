aa05f67edb3c34731d55867028a12f7be5240f4d
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-17 13:54:24 -0800

    [Kashyap Jhaveri] InHeritted the bill class and defined two computed properties for formatted start and end date and calculated total number of days and total instalments and total amount in init method and implemented the display function from the protocol

--- StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/Insurance.swift
+++ StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Insurance {
+class Insurance: Bill {
     var providerName:String;
     var type:String;
     var startDate:Date;
@@ -16,12 +16,36 @@ class Insurance {
     var totalDays:Int;
     var totalInstallment:Int;
     
-    init(_ providerName:String,_ type:String,_ startDate:Date,_ endDate:Date,_ totalDays:Int,_ totalInstallment:Int) {
+    var formatedStartDate:String{
+        let tempDateFormatter = DateFormatter();
+        tempDateFormatter.dateFormat = "dd-MMM-yyyy";
+        return tempDateFormatter.string(from: self.startDate);
+    }
+    
+    var formatedEndDate:String{
+        let tempDateFormatter = DateFormatter();
+        tempDateFormatter.dateFormat = "dd-MMM-yyyy";
+        return tempDateFormatter.string(from: self.endDate);
+    }
+    
+    init(_ billId:Int,_ billDate:Date,_ providerName:String,_ type:String,_ startDate:Date,_ endDate:Date) {
         self.providerName=providerName;
         self.type=type;
         self.startDate=startDate;
         self.endDate=endDate;
-        self.totalDays=totalDays;
-        self.totalInstallment=totalInstallment;
+        self.totalDays = Calendar.current.dateComponents([.day], from: startDate, to: endDate).day!;  //https://iostutorialjunction.com/2019/09/get-number-of-days-between-two-dates-swift.html
+        self.totalInstallment = Calendar.current.dateComponents([.month], from: startDate, to: endDate).month!;
+        let totalAmount = self.totalInstallment * 500;
+        super.init(billId, billDate, "Insurance", Float(totalAmount));
+    }
+    
+    override func Display() {
+        super.Display();
+        print("\tProvider Name:- \(self.providerName)");
+        print("\tInsurance Type:- \(self.type)");
+        print("\tStart Date:- \(self.formatedStartDate)");
+        print("\tEnd Date:- \(self.formatedEndDate)");
+        print("\tTotal Days:- \(self.totalDays)");
+        print("\tTotal Installments:- \(self.totalInstallment)");
     }
 }
