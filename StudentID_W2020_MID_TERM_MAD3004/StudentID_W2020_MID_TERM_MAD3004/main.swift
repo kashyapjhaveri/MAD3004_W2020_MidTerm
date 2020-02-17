3e369bcd4f8a1b5e19dc3a395187b8c26e1b2507
Kashyap Jhaveri <jhaverikashyap1998@gmail.com>
2020-02-17 14:07:15 -0800

    [Kashyap Jhaveri] 	1--> Added date format constant. 				2--> Validated the email and mobile number input. 				3--> Added method to init all the bills and assigned to customer class object 				4--> Formatted the output of all customers with their bill information

--- StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/main.swift
+++ StudentID_W2020_MID_TERM_MAD3004/StudentID_W2020_MID_TERM_MAD3004/main.swift
@@ -10,13 +10,16 @@ import Foundation
 
 var customers=[Int:Customer]();
 let mainMenuDesign = "+";
-let billMenuDesign = "*";
 var choice="0";
+
+let dateFormate = DateFormatter();
+dateFormate.dateFormat="dd/MM/yyyy";
 
 
 func initAllCustomers() {
     
     var tempCustomer:Customer;
+    
     var tempCustomerId:Int;
     var tempFirstName:String;
     var tempLastName:String;
@@ -30,9 +33,18 @@ func initAllCustomers() {
     tempEmail="jhaverikashyap1998@gmail.com";
     tempMobileNo="4379746768";
     
-    if tempEmail.isEmailValid() && tempMobileNo.isMobileNoValid() {
-        tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
-        customers.updateValue(tempCustomer, forKey: tempCustomerId);
+    if tempEmail.isEmailValid(){
+        if tempMobileNo.isMobileNoValid() {
+            tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
+            customers.updateValue(tempCustomer, forKey: tempCustomerId);
+        }
+        else
+        {
+            print("Mobile number \(tempMobileNo) not valid");
+        }
+    }
+    else{
+        print("Email \(tempEmail) not valid");
     }
     
     //Details of 2nd Customer
@@ -42,9 +54,18 @@ func initAllCustomers() {
     tempEmail="ravivagehla@gmail.com";
     tempMobileNo="7567549732";
     
-    if tempEmail.isEmailValid() && tempMobileNo.isMobileNoValid() {
-        tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
-        customers.updateValue(tempCustomer, forKey: tempCustomerId);
+    if tempEmail.isEmailValid(){
+        if tempMobileNo.isMobileNoValid() {
+            tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
+            customers.updateValue(tempCustomer, forKey: tempCustomerId);
+        }
+        else
+        {
+            print("Mobile number \(tempMobileNo) not valid");
+        }
+    }
+    else{
+        print("Email \(tempEmail) not valid");
     }
     
     //Details of 3rd Customer
@@ -54,9 +75,18 @@ func initAllCustomers() {
     tempEmail="nishantmoeala@gmail.c";
     tempMobileNo="9409521797";
     
-    if tempEmail.isEmailValid() && tempMobileNo.isMobileNoValid() {
-        tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
-        customers.updateValue(tempCustomer, forKey: tempCustomerId);
+    if tempEmail.isEmailValid(){
+        if tempMobileNo.isMobileNoValid() {
+            tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
+            customers.updateValue(tempCustomer, forKey: tempCustomerId);
+        }
+        else
+        {
+            print("Mobile number \(tempMobileNo) not valid");
+        }
+    }
+    else{
+        print("Email \(tempEmail) not valid");
     }
     
     //Details of 4th Customer
@@ -66,40 +96,109 @@ func initAllCustomers() {
     tempEmail="neelgotawala@gmail.com";
     tempMobileNo="8200399379";
     
-    if tempEmail.isEmailValid() && tempMobileNo.isMobileNoValid() {
-        tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
-        customers.updateValue(tempCustomer, forKey: tempCustomerId);
+    if tempEmail.isEmailValid(){
+        if tempMobileNo.isMobileNoValid() {
+            tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
+            customers.updateValue(tempCustomer, forKey: tempCustomerId);
+        }
+        else
+        {
+            print("Mobile number \(tempMobileNo) not valid");
+        }
+    }
+    else{
+        print("Email \(tempEmail) not valid");
+    }
+    
+    //Details of 5th Customer
+    tempCustomerId=5;
+    tempFirstName="Pruthvi";
+    tempLastName="Gandhi";
+    tempEmail="pruthvigandhi@gmail.com";
+    tempMobileNo="9409521797";
+    
+    if tempEmail.isEmailValid(){
+        if tempMobileNo.isMobileNoValid() {
+            tempCustomer = Customer(tempCustomerId,tempFirstName,tempLastName,tempMobileNo,tempEmail);
+            customers.updateValue(tempCustomer, forKey: tempCustomerId);
+        }
+        else
+        {
+            print("Mobile number \(tempMobileNo) not valid");
+        }
+    }
+    else{
+        print("Email \(tempEmail) not valid");
+    }
+}
+
+func initAllBills() {
+    var tempInternetObject:Internet;
+    var tempMobileObject:Mobile;
+    var tempHydroObject:Hydro;
+    
+    if let tempDate = dateFormate.date(from: "17/02/2020")
+    {
+        //Bill Information for 1st Customer
+        tempInternetObject = Internet(1, tempDate, "Bell", 100.0);
+        tempMobileObject = Mobile(2, tempDate, "Fido", "Medium", 10.0, 500.0)
+        if let tempCust = getCustomerById(Id: 1)
+        {
+            tempCust.addBill(bill: tempInternetObject);
+            tempCust.addBill(bill: tempMobileObject);
+        }
+        
+        //Bill Information for 2nd Customer
+        tempInternetObject = Internet(3, tempDate, "Rogers", 150.0);
+        tempMobileObject = Mobile(4, tempDate, "Chatr", "Medium", 10.0, 600.0)
+        tempHydroObject = Hydro(5, tempDate, "Toronto Hydro Crop", 100);
+        
+        if let tempCust = getCustomerById(Id: 2)
+        {
+            tempCust.addBill(bill: tempInternetObject);
+            tempCust.addBill(bill: tempMobileObject);
+            tempCust.addBill(bill: tempHydroObject);
+        }
+        
+        if let tempEndDate = dateFormate.date(from: "16/02/2021")
+        {
+            let tempInsuranceObject = Insurance(6, tempDate, "Aviva", "Car Insurance", tempDate, tempEndDate);
+            if let tempCust = getCustomerById(Id: 4)
+            {
+                tempCust.addBill(bill:tempInsuranceObject);
+            }
+        }
     }
 }
 
 
 func displayAllCustomers() {
-    
     for customer in customers.values
     {
+        mainMenuDesign.Repeat(n: 10);
         customer.Display();
+        print("\n");
+        mainMenuDesign.Repeat(n: 10);
+        print("\n");
     }
-    
 }
 
-func displayCustomerById(Id:Int) {
-    let tempCustomer = customers[Id] ?? nil
-    
-    if tempCustomer != nil {
-        tempCustomer?.Display();
-    }
-    else
+func getCustomerById(Id:Int) -> Customer? {
+    if let tempCustomer = customers[Id]
     {
-        print("Customer not found");
+        return tempCustomer;
     }
+    return nil;
 }
 
 
 //Execution starts from here
 initAllCustomers();
+initAllBills();
+
 
 mainMenuDesign.Repeat(n: 10);
-print("1.Display all customers");
+print("\n1.Display all customers");
 print("2.Display cutomer by ID")
 mainMenuDesign.Repeat(n: 10);
 
@@ -112,19 +211,25 @@ switch choice
     case "1":
         displayAllCustomers();
     break;
-    
+
     case "2":
         print("Enter customer id:- ");
         if let custId = Int(readLine() ?? "0")
         {
-            displayCustomerById(Id: custId)
+            if let tempCust = getCustomerById(Id: custId)
+            {
+                tempCust.Display();
+            }
+            else{
+                print("Customer with ID \(custId) not found")
+            }
+        }
+        else
+        {
+            print("Customer ID should be number only");
         }
     break;
-    
-    case "3":
-        
-    break;
-    
+
     default:
     break;
 }
